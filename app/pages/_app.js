6f4e0e4ac56ad3563010d4528535d5d9b62c6e92
AlexCPT-cpu <alexodey79@gmail.com>
2024-05-07 02:24:08 +0100

    added sol transfer

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -6,13 +6,16 @@ import {
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
 import "@/styles/globals.css";
+import { clusterApiUrl } from "@solana/web3.js";
 import { endpoint } from "@/constants";
+
+const rpcEndpoint = clusterApiUrl("devnet"); // Specify the Solana network
 
 export default function App({ Component, pageProps }) {
   const phantomWallet = new PhantomWalletAdapter();
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={[phantomWallet]}>
+      <WalletProvider wallets={[phantomWallet]} autoConnect>
         <WalletModalProvider>
           <Component {...pageProps} />
         </WalletModalProvider>
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,4 +1,13 @@
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { WalletNotConnectedError } from "@solana/wallet-adapter-base";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import React, { FC, useCallback, useEffect } from "react";
 import useMount from "@/hooks/useMount";
 import { useState } from "react";
 import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
@@ -6,9 +15,55 @@ import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 
 export default function Home() {
   const mounted = useMount();
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction, wallet } = useWallet();
   const [amount, setAmount] = useState(0); // State for input amount
   const [totalTokensBought, setTotalTokensBought] = useState(0); // State for total tokens bought
   const [tokensReleased, setTokensReleased] = useState(0); // State for released tokens
+  const [recipientAddress, setRecipientAddress] = useState(
+    "HmwhDijzFs1uBw7h8yWqcZvhLNUuJR61sSidRDabp7kz"
+  );
+
+  useEffect(() => {
+    const tokens = localStorage.getItem("tokens_bought");
+    setTotalTokensBought(tokens);
+    setTokensReleased(0.2 * Number(tokens));
+  }, []);
+
+  const handleTransfer = async () => {
+    // if (!wallet.connected) {
+    //   // Handle case where wallet is not connected
+    //   alert("Please Connect Your Wallet");
+    //   return;
+    // }
+    // if (!publicKey) throw new WalletNotConnectedError();
+    if (!amount || amount <= 0) {
+      alert("Please Enter an amount");
+      return;
+    }
+    const recipientPublicKey = new PublicKey(recipientAddress);
+    const lamports = parseInt(amount) * 1000000000; // Convert SOL to lamports
+    const transaction = new Transaction().add(
+      SystemProgram.transfer({
+        fromPubkey: publicKey,
+        toPubkey: recipientPublicKey,
+        lamports: lamports,
+      })
+    );
+    try {
+      // Construct and sign transaction
+
+      const signature = await sendTransaction(transaction, connection);
+
+      await connection.confirmTransaction(signature, "processed");
+      // Handle successful transaction
+      console.log("Transaction sent:", signature);
+      localStorage.setItem("tokens_bought", String(amount));
+    } catch (error) {
+      // Handle transaction error
+      console.error("Transaction failed:", error);
+    }
+  };
 
   const handleBuyTokens = () => {
     // Add your logic here to handle buying tokens
@@ -58,12 +113,18 @@ export default function Home() {
         {/* Counter for token sale */}
         <div className="mt-10 mb-16">
           <div className="border border-[#512da8] rounded-lg p-4 mb-4">
-            <p className="text-lg">Total Tokens Bought: {totalTokensBought}</p>
+            <p className="text-lg">
+              Total Tokens Bought: {totalTokensBought ? totalTokensBought : 0}
+            </p>
           </div>
           <div className="border border-green-500 rounded-lg p-4">
-            <p className="text-lg">Tokens Released (TGE): {tokensReleased}</p>
+            <p className="text-lg">
+              Tokens Released (TGE): {tokensReleased ? tokensReleased : 0}
+            </p>
           </div>
         </div>
+
+        <div>Price per token: 0.1 SOl</div>
 
         {/* Input field for user to input amount */}
         <div className="flex flex-col items-center mb-4 mt-2 lg:mt-10">
@@ -74,15 +135,15 @@ export default function Home() {
             type="number"
             id="amount"
             className="bg-gray-700 text-white px-4 py-2 rounded-md w-64 outline-none appearance-none hover:appearance-none"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
+            //value={amount}
+            onChange={(e) => setAmount(Number(e.target.value) * 0.1)}
           />
         </div>
 
         {/* Button to buy tokens */}
         <button
           className="bg-[#512da8] text-white px-9 py-3 rounded-md shadow-md hover:bg-gray-900 transition duration-300"
-          onClick={handleBuyTokens}
+          onClick={handleTransfer}
         >
           Buy Tokens
         </button>
